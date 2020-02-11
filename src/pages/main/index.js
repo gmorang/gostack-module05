0cdf478520510cb290ec3c2eb9beef7055668876
Gabriel Morandim <augustomorandim@gmail.com>
2020-02-11 14:35:33 -0300

    add css and add repo

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa'
-
+import api from '../../services/api';
 import { Container, Form, SubmitButton } from './style'
 
 function Main() {
+  const [repos, setRepos] = React.useState([]);
+  const [text, setText] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const response = await api.get(`/repos/${text}`)
+
+    const data = {
+      name: response.data.full_name
+    }
+
+    setRepos([...repos, data]);
+    setLoading(false);
+    setText('');
+  }
+
   return (
     <Container>
       <h1><FaGithubAlt />Repositories</h1>
-      <Form onSubmit={() => { }}>
-        <input type="text" placeholder="Adicionar repositiorio" />
-        <SubmitButton disable>
-          <FaPlus color="#FFF" size={14} />
+      <Form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Adicionar repositiorio" value={text} onChange={e => setText(e.target.value)} />
+        <SubmitButton loading={isLoading}>
+          {!isLoading ? <FaPlus color="#FFF" size={14} /> : <FaSpinner color="#FFF" size={14} />}
         </SubmitButton>
       </Form>
 
